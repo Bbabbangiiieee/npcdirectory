adbd3b1a724bba1cfdb5e94ed76c945bb9540250
Bbabbangiiieee <christinamae.gerzon@g.msuiit.edu.ph>
2024-06-05 18:11:26 +0800

    auth

---
+++ src/index.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCAScRRCt3vkzG9kbaaVlwn1S6R2PkSZt8",
+  authDomain: "npc-directory-fe968.firebaseapp.com",
+  projectId: "npc-directory-fe968",
+  storageBucket: "npc-directory-fe968.appspot.com",
+  messagingSenderId: "405924419354",
+  appId: "1:405924419354:web:886dbdd295db21d008bea3"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
